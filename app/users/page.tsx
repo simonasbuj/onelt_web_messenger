159893c0f66abc5e74a00787c6a31f6f243df851
unknown <simonas.b@hotmail.com>
2023-06-27 20:49:34 +0300

    quick fix for gha

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -9,7 +9,7 @@ const Users = () => {
     return (
         <>
             <div className="flex flex-col items-center">
-                <div className="w-3/4 text-center">you're logged in, good job</div>
+                <div className="w-3/4 text-center">youre logged in, good job</div>
                 <button 
                     className="font-bold mt-4 border-4 border-orange-400 rounded-lg py-2 px-4 hover:bg-orange-400"
                     onClick={() => signOut({ callbackUrl: '/' })}
